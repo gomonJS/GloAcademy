1b26d97b822b0e1e201ff7c3fd828ca36cb329c5
Александр Гомон <28gomon@mail.ru>
2020-01-25 14:13:50 +0300

    Ветка lesson02

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
-let money;
-let income; 
-let addExpenses;
-let deposit; 
-let mission; 
-let period;
+/*
+1) Следующим переменным присвоить значения 
+   - money - любое число “Доход за месяц”,
+   - income - строка с дополнительными доходом (например: фриланс), 
+   - addExpenses - строка с перечислением дополнительных расходов через запятую (например: интернет, такси, коммуналка), 
+   - deposit - любое булево значение,
+   - mission - любое число (Какую сумму хотите накопить),
+   - period - число от 1 до 12 (месяцев)
+2) Используя методы и свойства:
+   - Вывести в консоль тип данных значений переменных money, income, deposit;
+   - Вывести в консоль длину строки addExpenses
+   - Вывести в консоль “Период равен (period) месяцев” и “Цель заработать (mission) рублей/долларов/гривен/юани”
+   - Привести строку addExpenses к нижнему регистру и разбить строку на массив, вывести массив в консоль
+   - Объявить переменную budgetDay и присвоить дневной бюджет (доход за месяц / 30)
+   - Вывести в консоль budgetDay
+*/
 
-alert('GloAcademy!');
-console.log('Сообщение в консоль.');
-console.log('Работаю с Git!');
+let money = 2500,
+    income = 'фриланс',
+    addExpenses = 'Тренажерный, Машина, Комуналка, Интернет',
+    deposit = true,
+    mission = 1200,
+    period = 1;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+
+let strPeriod = `Период равен '${period}' месяцев`;
+let strMission = `Цель заработать '${mission}' рублей/долларов/гривен/юани`
+
+console.log(strPeriod);
+console.log(strMission);
+
+let lowExpenses = addExpenses.toLowerCase();
+
+console.log(lowExpenses.split(', '));
+
+let budgetDay = money / 30;
+console.log(budgetDay.toFixed(2));

24604f08ece48a7146634bf69ef46433763405bb
Александр Гомон <28gomon@mail.ru>
2020-01-27 11:34:43 +0300

    Добавлен файл jahintrc

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-let money,
-    income,
-    addExpenses,
-    deposit, 
-    mission, 
-    period;
-
-alert('GloAcademy!');
-console.log('Сообщение в консоль.');
-console.log('Работаю с Git!');
+'use strict';

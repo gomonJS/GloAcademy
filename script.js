08f0ab78401a01de95aabb96955841c6bb0bc3e9
Александр Гомон <28gomon@mail.ru>
2020-02-01 16:26:30 +0300

    Новая правка

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 let week = [
-    'Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота' 
+    'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'
 ];
 
 let blockWeek = document.getElementById('blockWeek'),
@@ -11,9 +11,9 @@ for (let i = 0; i < week.length; i++) {
 
     if (day === i) {
         blockWeek.innerHTML += '<p><strong>' + week[i] + '</strong></p>';
-    } else if (i === 6) {
+    } else if (i === 5) {
         blockWeek.innerHTML += '<p><em>' + week[i] + '</em></p>';
-    } else if (i === 0) {
+    } else if (i === 6) {
         blockWeek.innerHTML += '<p><em>' + week[i] + '</em></p>';
     } else {
         blockWeek.innerHTML += '<p>' + week[i] + '</p>';

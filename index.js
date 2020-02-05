74d004bb6c2948efcf7f08b7cb27823517a98404
Александр Гомон <28gomon@mail.ru>
2020-02-05 12:50:06 +0300

    Добавил недостающие элементы

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 'use strict';
 
 const startButton = document.getElementById('start'),
+        cancelButton = document.getElementById('cancel'),
         buttonPlusIncome = document.getElementsByTagName('button')[0],
         buttonPlusExpenses = document.getElementsByTagName('button')[1],
         depositCheckbox = document.querySelector('#deposit-check'),
         additionalIncomeItem = document.querySelectorAll('.additional_income-item'),
         expensesTitle = document.querySelector('input.expenses-title'),
         expensesAmount = document.querySelector('input.expenses-amount'),
-        resultTotalList = document.querySelectorAll('.result-total'),
+        budgetMonthValue = document.getElementsByClassName('budget_month-value'),
+        budgetDayValue = document.getElementsByClassName('budget_day-value'),
+        expensesMonthValue = document.getElementsByClassName('expenses_month-value'),
+        additionalIncomeValue = document.getElementsByClassName('additional_income-value'),
+        additionalExpensesValue = document.getElementsByClassName('additional_expenses-value'),
+        incomePeriodValue = document.getElementsByClassName('income_period-value'),
+        targetMonthValue = document.getElementsByClassName('target_month-value'),
         salaryAmount = document.querySelector('.salary-amount'),
         incomeTitle = document.querySelector('.income-title'),
         incomeAmount = document.querySelector('.income-amount'),
         additionalExpensesItem = document.querySelector('.additional_expenses-item'),
         periodSelect = document.querySelector('.period-select');
-
-console.log(buttonPlusIncome);
-console.log(buttonPlusExpenses);
